40873fc2d12e22bb54aed77b9ab6241caaa962ac
Nanda Kishore <madhav.bnk@gmail.com>
2012-05-11 09:07:16 +0530

    Initial Version implementing Interfaces

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='design-patterns-tryouts',
+    version='1.0.0',
+    long_description=open('README.md', 'r').read(),
+    author='NandaKishore',
+    author_email='madhav.bnk@gmail.com',
+    packages=find_packages(exclude=["tests"]),
+    install_requires=['zope.interface',
+                      'zope.component',
+                      ],
+    )
---
+++ src/reader.py
@@ -0,0 +1,56 @@
+import csv
+import os
+from zope.interface import Interface, Attribute, implements
+from lxml import etree
+
+class IReader(Interface):
+    '''
+    This Reader should be programmed to have different implementations.
+    Like Reading a CSV file, XML file etc...
+    '''
+    name = Attribute('''Name/Path of the file''')
+
+    def open():
+        '''
+        This method opens the file for reading
+        '''
+
+    def close():
+        '''
+        This method closes the file after performing operations
+        to the file.
+        '''
+
+    def reader():
+        '''
+        This method iterates on the lines of the file.
+        It could be iterating on
+        1. On individual records if its a csv file.
+        2. On a specific node if its a xml file.
+        3. etcetra....
+        '''
+
+class CSVFileReader(object):
+    implements(IReader)
+
+    def __init__(self, name):
+        self.name = name
+
+    def reader(self):
+        return csv.reader(open(os.path.abspath(self.name), 'r'),
+                          delimiter=',',
+                          quotechar='"')
+
+class XMLFileReader(object):
+    implements(IReader)
+
+    def __init__(self, name):
+        self.name = name
+
+    def reader(self):
+        etree.iterparse()
+
+if __name__ == '__main__':
+    fl = CSVFileReader(name='sample.csv')
+    for line in fl.reader():
+        print line
